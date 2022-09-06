b17e084f3aeecbaf3b1fb4ddad26d7e2439ad82e
Jeffrey <jeff.ho@hotmail.com>
2022-09-06 14:35:26 -0400

    Add days left conditionals

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -12,10 +12,16 @@ export default function DayListItem(props) {
   'day-list__item--full':props.spots === 0
   });
 
+  const formatSpots = (
+    (props.spots === 0 && <h3 className="text--light">no spots remaining</h3> ) ||
+    (props.spots === 1 && <h3 className="text--light">1 spot remaining</h3> ) ||
+    (props.spots  && <h3 className="text--light">{props.spots} spots remaining</h3> )
+    );
+
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots}</h3>
     </li>
   );
 } 
