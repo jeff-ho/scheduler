6b28ffdbd18ba308f74b88467f9967d880aec769
Jeffrey <jeff.ho@hotmail.com>
2022-09-07 13:18:40 -0400

    Add story and components for appointment

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import InterviewerListItem from './InterviewerListItem';
 import 'components/InterviewerList.scss';
+import { useState } from 'react';
 
-export default function InterviewerList(props) {
+export default function InterviewerList({value, onChange, interviewers}) {
 
 
-  const interviewers = props.interviewers.map(interviewer => {
+  const interviewersList = interviewers.map(({id, name, avatar})=> {
     return(
     <InterviewerListItem 
-    key={interviewer.id}
-    name={interviewer.name}
-    avatar={interviewer.avatar}
-    selected={props.interviewer === interviewer.id}
-    setInterviewer={() => props.setInterviewer(interviewer.id)}
+    key={id}
+    name={name}
+    avatar={avatar}
+    selected={value === id}
+    setInterviewer={() => onChange(id)}
     />
     )
   });
@@ -20,7 +21,7 @@ export default function InterviewerList(props) {
   return (
     <section className="interviewers">
     <h4 className="interviewers__header text--light">Interviewer</h4>
-    <ul className="interviewers__list">{interviewers}</ul>
+    <ul className="interviewers__list">{interviewersList}</ul>
     </section>
   )
 }
