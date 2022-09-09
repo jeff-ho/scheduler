4ce6c5c93e89a32211d0ccf63733f991397d3bcb
Jeffrey <jeff.ho@hotmail.com>
2022-09-09 16:34:24 -0400

    Implement working CREATE schedule transition as well as working cancel schedule button

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,15 +3,32 @@ import 'components/Appointment/styles.scss';
 import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
+import useVisualMode from 'hooks/useVisualMode';
+import Form from './Form';
 
 export default function Appointment(props) {
-  
-  const render = props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer.name}/> : <Empty/>
-   
+  const EMPTY = 'EMPTY';
+  const SHOW = 'SHOW';
+  const CREATE = 'CREATE';
+  const { mode, transition, back} = useVisualMode (
+    props.interview ? SHOW : EMPTY
+  );
+
+
+
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {render}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && 
+      <Show
+      student={props.interview.student}
+      interviewer={props.interview.interviewer.name}
+      />
+      }
+      {mode === CREATE && <Form student='' interviewers={[]} onCancel={() => back()} />}
+
+
     </article>
   )
 }
